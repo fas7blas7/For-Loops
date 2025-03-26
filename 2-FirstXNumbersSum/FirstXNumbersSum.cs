dcd990e00b67c4a52c74159bd515f1a00e606d52
Martin Kirov <fas7blas7@gmail.com>
2025-03-26 20:57:21 +0200

    Adding FirstXNumbersSum task

---
+++ 2-FirstXNumbersSum/FirstXNumbersSum.cs
@@ -0,0 +1,27 @@
+﻿namespace _2_FirstXNumbersSum
+{
+    internal class FirstXNumbersSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+
+                if(i < n)
+                {
+                    Console.Write(i + "+");
+                }
+                else
+                {
+                    Console.Write(i + "=");
+                }
+            }          
+
+            Console.WriteLine(sum);
+        }
+    }
+}
