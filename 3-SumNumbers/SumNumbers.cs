e99ec77159c2879b11eb2424b8216a2c64ababde
Martin Kirov <fas7blas7@gmail.com>
2025-03-26 20:57:46 +0200

    Adding SumNumbers task

---
+++ 3-SumNumbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿namespace _3_SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                double currentNumber = double.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
