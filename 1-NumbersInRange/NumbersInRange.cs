f90bb62fddab213ddc45c808e7784c993bbcb550
Martin Kirov <fas7blas7@gmail.com>
2025-03-26 20:56:40 +0200

    Adding NumbersInRange task

---
+++ 1-NumbersInRange/NumbersInRange.cs
@@ -0,0 +1,16 @@
+﻿namespace _1_NumbersInRange
+{
+    internal class NumbersInRange
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
