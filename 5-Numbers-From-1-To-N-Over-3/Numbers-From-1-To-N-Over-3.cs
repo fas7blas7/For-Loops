caab7b7d5178fae40575a0af9a14fa571e699c0c
Martin Kirov <skylinecho@abv.bg>
2025-03-27 23:02:18 +0200

    Adding NumbersFrom1ToNOver3 task

---
+++ 5-Numbers-From-1-To-N-Over-3/Numbers-From-1-To-N-Over-3.cs
@@ -0,0 +1,15 @@
+﻿namespace _5_Numbers_From_1_To_N_Over_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
