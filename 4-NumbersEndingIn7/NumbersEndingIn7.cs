099468b633738e759e8e2ec112c162a4aabc6408
Martin Kirov <fas7blas7@gmail.com>
2025-03-27 22:23:13 +0200

    Adding NumbersEndingIn7 task

---
+++ 4-NumbersEndingIn7/NumbersEndingIn7.cs
@@ -0,0 +1,18 @@
+﻿namespace _4_NumbersEndingIn7
+{
+    internal class NumbersEndingIn7
+    {
+        static void Main(string[] args)
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= endOfRange; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
