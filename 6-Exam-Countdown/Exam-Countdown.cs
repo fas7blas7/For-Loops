3ce125135e7e2b64b281f5bec9ca74fa99d85fd7
Martin Kirov <skylinecho@abv.bg>
2025-03-27 23:04:06 +0200

    Adding ExamCountdown task

---
+++ 6-Exam-Countdown/Exam-Countdown.cs
@@ -0,0 +1,17 @@
+﻿namespace _6_Exam_Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysToExam = int.Parse(Console.ReadLine());
+
+            for (int day = daysToExam; day >= 1; day--)
+            {
+                Console.WriteLine($"{day} days before the exam");
+            }
+
+            Console.WriteLine("The exam has come");
+        }
+    }
+}
